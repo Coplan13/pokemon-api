9a7e9a8847ff24e8c23a61bfb535a4dfbca21a22
Coplan13 <sam.poirier37@gmail.com>
2019-06-13 12:16:08 +0200

    authentification entete

--- app/controllers/ControllerTest.java
+++ app/controllers/ControllerTest.java
@@ -1,21 +1,30 @@
 package controllers;
 
 import com.auth0.jwt.JWT;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import dao.*;
 import models.*;
 import play.api.Play;
+
 import play.data.DynamicForm;
 import play.data.Form;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+
+
+import scala.util.parsing.json.JSON;
+import scala.util.parsing.json.JSONObject;
 import token.TokenManager;
 import uk.co.panaxiom.playjongo.PlayJongo;
 
 
 import javax.inject.Inject;
+import java.awt.*;
 
 public class ControllerTest extends Controller {
 
@@ -26,6 +35,12 @@ public class ControllerTest extends Controller {
         this.profileDAO = new ProfileDAO(playJongo);
     }
 
+    public Result map(Http.Request request)
+    {
+       String tok =  request.getHeaders().get("Beamer").toString();
+
+        return ok(tok);
+    }
 
 
     public Result login(Http.Request request) {
@@ -42,7 +57,22 @@ public class ControllerTest extends Controller {
 
             String token = tokenManager.generateToken(email);
 
-            return ok("Le token : "+token);
+            ObjectNode profil = Json.newObject();
+            profil.put("_id",profile._id);
+            profil.put("username",profile.login);
+            profil.put("picture",profile.picture);
+            profil.put("email",profile.email);
+            profil.put("candy",0);
+
+            ObjectNode result = Json.newObject();
+
+            result.put("profil",profil);
+
+
+            result.put("token", token);
+
+
+            return ok(result);
         }
         else
         {
